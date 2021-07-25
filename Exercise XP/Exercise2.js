8d36a91f3d5d137fa1d2422b7d9f66d15d8a5589
OferHeiman <87015903+OferHeiman@users.noreply.github.com>
2021-07-25 07:25:03 +0300

    Week 3 Day 4 Exercises XP
    Week 3 Day 4 Exercises XP

---
+++ Exercise XP/Exercise2.js
@@ -0,0 +1,12 @@
+let newDog = "Chihuahua"; //1
+console.log(newDog.length); //2
+console.log(newDog.toUpperCase()); //3
+console.log(newDog.toLowerCase()); //3
+if(newDog == "Chihuahua")//4
+{
+    console.log("I love Chihuahuas, it’s my favorite dog breed");
+}
+else
+{
+    console.log("I dont care, I prefer cats");
+}
