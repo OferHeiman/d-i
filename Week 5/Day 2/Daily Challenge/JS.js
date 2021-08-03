8fc1f4101b6926a2549c31a4a278d55f138f4d8d
OferHeiman <oferheiman@gmail.com>
2021-08-03 23:00:57 +0300

    Updated code

--- Week 5/Day 2/Daily Challenge/JS.js
+++ Week 5/Day 2/Daily Challenge/JS.js
@@ -6,8 +6,14 @@ function tellStory() {
     let input3Text = document.getElementById('person').value;
     let input4Text = document.getElementById('verb').value;
     let input5Text = document.getElementById('place').value;
-    let paragraph=document.getElementById('story');
-    let story = "An " + input1Text + " by the name of " + input3Text + "," + input4Text + " on planet " + input5Text + ",a " + input2Text + " landscape.";
-    //example noun=astronaut,adjective=hellish,person=Selene,verb=crashed,place=Atropos
-    paragraph.innerText = story;
+    let paragraph = document.getElementById('story');
+    if (input1Text.length == 0 || input2Text.length == 0 || input3Text.length == 0 || input4Text.length == 0 || input5Text.length == 0) {
+        alert("Please make sure all fields are filled");
+        return;
+    }
+    else {
+        let story = "An " + input1Text + " by the name of " + input3Text + "," + input4Text + " on planet " + input5Text + ",a " + input2Text + " landscape.";
+        //example noun=astronaut,adjective=hellish,person=Selene,verb=crashed,place=Atropos
+        paragraph.innerText = story;
+    }
 }
