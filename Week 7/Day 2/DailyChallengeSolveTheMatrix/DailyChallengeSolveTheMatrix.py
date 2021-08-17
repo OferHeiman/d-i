b4fba75e15fab08510b2511c2a7b70f17e974494
OferHeiman <oferheiman@gmail.com>
2021-08-17 18:40:30 +0300

    Update DailyChallengeSolveTheMatrix.py

--- Week 7/Day 2/DailyChallengeSolveTheMatrix/DailyChallengeSolveTheMatrix.py
+++ Week 7/Day 2/DailyChallengeSolveTheMatrix/DailyChallengeSolveTheMatrix.py
@@ -1,6 +1,7 @@
+# My Solution
 import re
 matrix=[
-    [7,'i',3],
+    ['7','i','3'],
     ['T','s','i'],
     ['h','%','x'],
     ['i',' ','#'],
@@ -18,3 +19,28 @@ def decrypt_matrix(matrix):
     matrix_string = re.sub("[^a-zA-Z\s]","",matrix_string)
     print(matrix_string)  
 decrypt_matrix(matrix)
+
+# Ayal's solution
+# matrix = [
+# 		['7', 'i', '3'],
+# 		['T', 's', 'i'],
+# 		['h', '%', 'x'],
+# 		['i', ' ', '#'],
+# 		['s', 'M', ' '],
+# 		['$', 'a', ' '],
+# 		['#', 't', '%'],
+# 		['^', 'r', '!'],
+# ]
+
+# result = ''
+# num_of_column = len(matrix[0])
+# num_of_rows = len(matrix)
+# for i in range(num_of_column):
+# 	for j in range(num_of_rows):
+# 		# scan the column
+# 		if matrix[j][i].isalpha():
+# 			result += matrix[j][i]
+# 		elif result and result[-1] != ' ':  # result is not an empty string and the last item is not a space
+# 			result += ' '
+
+# print(result)
