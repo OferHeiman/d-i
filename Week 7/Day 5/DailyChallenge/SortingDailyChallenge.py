daabaa3d655f8fb378a0ab30c2652ef7bc7abd7d
OferHeiman <oferheiman@gmail.com>
2021-08-21 23:25:38 +0300

    Added Daily Challenge Sorting

---
+++ Week 7/Day 5/DailyChallenge/SortingDailyChallenge.py
@@ -0,0 +1,6 @@
+def sort_list_alphabetically():
+    sequence_of_words = input("Type a comma separated sequence of words: ")
+    sequence_of_words = sequence_of_words.split(',')
+    sequence_of_words.sort()
+    print(','.join(sequence_of_words))
+sort_list_alphabetically()
