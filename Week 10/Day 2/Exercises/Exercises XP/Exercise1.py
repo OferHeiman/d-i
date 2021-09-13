51f564d363ab78a1809f4da68232d668fcd1c4b7
OferHeiman <oferheiman@gmail.com>
2021-09-13 21:02:43 +0300

    Exercises XP

--- Week 10/Day 2/Exercises/Exercises XP/Exercise1.py
+++ Week 10/Day 2/Exercises/Exercises XP/Exercise1.py
@@ -2,3 +2,7 @@ import operators
 from operators import addOperator 
 from operators import divideOperator as divide 
 import operators as ops
+
+print(addOperator(5,7))
+print(divide(9,3))
+print(ops.addOperator(10,15))
