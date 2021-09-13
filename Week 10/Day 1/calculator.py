ada9c8d235ea1cb96d405dd2be374ca42c5ff0db
OferHeiman <oferheiman@gmail.com>
2021-09-13 20:47:31 +0300

    exercises

---
+++ Week 10/Day 1/calculator.py
@@ -0,0 +1,8 @@
+# import operators
+# from operators import addOperator
+# from operators import divideOperator
+from operators import addOperator as add
+from operators import divideOperator as divide
+
+print(add(27,23))
+print(divide(10,5))
