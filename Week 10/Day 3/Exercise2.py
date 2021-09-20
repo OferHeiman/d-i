bf2c2e5ee35722580c13726b0fb1632ca9c87e7a
OferHeiman <oferheiman@gmail.com>
2021-09-20 17:07:14 +0300

    Update Exercise2.py

--- Week 10/Day 3/Exercise2.py
+++ Week 10/Day 3/Exercise2.py
@@ -1,20 +1,15 @@
 import json
 
-# my_family = {
-#     "parents":['Beth', 'Jerry'],
-#     "children":['Summer', 'Morty']
-# }
+my_family = {
+    "parents":['Beth', 'Jerry'],
+    "children":['Summer', 'Morty']
+}
 
-# json_file = "my_file.json"
+json_file = "my_file.json"
 
-# with open(json_file, 'w') as file_obj:
-#     json.dump(my_family, file_obj)
-#    #json.dump(obj2save , destination_file)
+with open(json_file, 'w') as file_obj:
+    json.dump(my_family, file_obj)
+   #json.dump(obj2save , destination_file)
 
-# my_family = {
-#     "parents" :['Beth', 'Jerry'],
-#     "children" :['Summer', 'Morty']
-# }
-
-# json_my_family = json.dumps(my_family)
-# print(json_my_family)
+json_my_family = json.dumps(my_family)
+print(json_my_family)
